c15ffb67f1beebf807ca57455b720a2df50ae7bb
bernat <elbernat@gmail.com>
2020-10-09 11:41:50 -0400

    Vue3 - first steps

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 // JS 
 console.log("main.js connected!");
+
+// Vue Counter
+const Data = {
+    data() {
+        return {
+            counter: 0,
+            date : new Date().toLocaleString()
+        }
+    },
+    mounted() {
+        setInterval( ()=> {
+            this.counter++;
+            // console.log(this.counter);
+        },1000);
+        
+    }
+}
+
+Vue.createApp(Data).mount('#data')
